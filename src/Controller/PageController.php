6e31334c4741265b7e6586a1a3c5797dbf27ad74
maxijayme <maxijayme@gmail.com>
2023-10-29 17:35:02 +0100

    manejar que ocurre una vez enviado el formulario

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -14,16 +15,21 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 class PageController extends AbstractController
 {
     #[Route('/contactos-v1', methods:['GET', 'POST'])]
-    public function contactV1(): Response
+    public function contactV1(Request $request): Response
     {
         $form = $this->createFormBuilder()
                 ->add('email', TextType::class)
                 ->add('message', TextareaType::class, ['label'=>'Comentarios, sugerencias, etc...'])
                 ->add('agreeTerms', CheckboxType::class)
                 ->add('save', SubmitType::class,['label'=>'Enviar'])
-                ->setMethod('GET')
-                ->setAction('otra_url')
+                // ->setMethod('GET')
+                // ->setAction('otra_url')
                 ->getForm();
+        
+        $form->handleRequest($request);        
+        if($form->isSubmitted()){
+            dd($form->getData(), $request);
+        }
 
         return $this->render('page/contact-v1.html.twig', [
             'form' => $form ->createView(),
