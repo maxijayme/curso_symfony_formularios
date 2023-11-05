52c426796729b59bb1f7ff35bc35446ec58a4523
maxijayme <maxijayme@gmail.com>
2023-11-05 15:52:06 +0100

    CSRF protection activated and some input properties

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,18 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Email')
-            ->add('Message')
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'help' => 'Debe contener mínimo 8 caracteres'
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Mensaje',
+                'attr' => ['rows' => 5, 'class' => 'bg-light']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enviar',
+                'attr' => ['class' => 'btn btn-primary']
+            ])
         ;
     }
 
